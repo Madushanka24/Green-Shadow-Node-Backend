c4909860f1765a654921c5b4177fa4e479902ec6
Madushanka24 <madushanka20021224@gmail.com>
2025-02-26 19:58:25 +0530

    Crop Modle

---
+++ modle/Crop.ts
@@ -0,0 +1,9 @@
+export default class Crop {
+    cropId!: string;
+    cropName!: string;
+    cropScientificName!: string;
+    cropCategory!: string;
+    cropSeason!: string;
+    cropImage1!: string;
+    cropFieldId!: string;
+}
