75591a25573301c0974cd1698c27e5b3170db084
Madushanka24 <madushanka20021224@gmail.com>
2025-02-26 20:55:18 +0530

    user route

--- modle/Log.ts
+++
@@ -1,9 +0,0 @@
-export default class Log {
-    logCode!: string;
-    logDate!: string;
-    details!: string;
-    observedImage!: string;
-    logFieldId!: string;
-    logCropId!: string;
-    logStaffId!: string;
-}
